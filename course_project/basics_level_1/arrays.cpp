3baec48d340aaf9460015600ac6fea8c45e02bb8
NanoStealth88 <chandler.bartholomew@gmail.com>
2022-12-11 10:34:21 -0600

    Adding new arrays.cpp file to practice with arrays

---
+++ course_project/basics_level_1/arrays.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    double temperature_list[4] = {34.5, 27.8, 26.8, 22.0};
+
+    std::cout << temperature_list[1] << std::endl;
+    return 0;
+}
--- course_project/basics_level_1/hello.cpp
+++ course_project/basics_level_1/hello.cpp
@@ -2,6 +2,17 @@
 
 int main()
 {
-    std::cout << "Hello" << std::endl;
+    const int seconds_per_hour = 3600;              //has to be initialized in the same declarative statement
+    const double max_allowed_temperature = 76.8;    //has to be initialized when declared
+
+    int user_age = 34;
+    double temperature = 20.6;
+    bool is_alive = false;
+    std::string user_name = "Bob";
+    
+    std::cout << user_age << std::endl;
+    std::cout << temperature << std::endl;
+    std::cout << is_alive << std::endl;
+    std::cout << user_name << std::endl;
     return 0;
-}
+} 
