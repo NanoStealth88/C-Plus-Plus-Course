af50e211909fafc186ef6f582836964087a47d13
NanoStealth88 <chandler.bartholomew@gmail.com>
2022-12-11 23:45:03 -0600

    Made changes to the interface of the program to call out when the input is being asked for

--- course_project/basics_level_1/Exercise-1-2.cpp
+++ course_project/basics_level_1/Exercise-1-2.cpp
@@ -6,9 +6,12 @@ int main ()
     int number2;
     
     std::cout << "Please enter two integer numbers: " << std::endl;
-    std::cin >> number1 >> number2;
-
-    std::cout << "The sum is " << number1 + number2 << std::endl;
+    std::cout << "Enter the first number: ";
+    std::cin >> number1;
+    std::cout << "Enter the second number: ";
+    std::cin >> number2;
+    int sum = number1 + number2;
+    std::cout << "The sum of " << number1 << " and " << number2 << " is " << sum << std::endl;
 
     return 0;
 }
